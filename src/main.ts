e26e33332129ce3a407dde0d7fe1aaff89f49b3f
planetabhi <abhimanyu.rana@postman.com>
2023-12-01 00:12:21 +0530

    setup & polishing

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import logo from './sargam.svg'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <a href="https://sargam.design">
-      <img src="${logo}" class="logo" alt="Sargam logo" />
+      <img src="${logo}" class="logo" alt="Sargam logo" width="106px" height="106px" />
     </a>
   </div>
 `
